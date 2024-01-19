0b3d61d3aa7d7eba8aece705bbec64abf5d67bea
Greg Hall <greghall@bantling.me>
2024-01-19 08:58:42 -0400

    union registers Maybe as a wrapper

--- union/conv/init.go
+++ union/conv/init.go
@@ -2,6 +2,12 @@ package conv
 
 // SPDX-License-Identifier: Apache-2.0
 
-func init() {
+import (
+  "github.com/bantling/micro/conv"
+)
 
+func init() {
+  // Register the MaybeWrapperInfo so conv.To can handle Maybe wrappers
+  var mwi MaybeWrapperInfo
+  conv.MustRegisterWrapper(mwi)
 }
